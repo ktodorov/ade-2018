05bda07ee0fa3ae83f8a1becf3d1440dc1dd084d
ozzyou <ozzy.ulger@gmail.com>
2018-10-20 14:50:22 +0200

    added visitor data mock

--- backend/DepartPoints.py
+++ backend/DepartPoints.py
@@ -1,8 +1,30 @@
 import numpy as np
+import csv
 
+N_VISITORS = 45000
 def visiters_per_city(visitors):
     uniques, counts = np.unique(visitors, return_counts=True)
     uniquecounts = zip(uniques, counts)
 
-# example list of visitors
-visitors = np.array(["amsterdam", "amsterdam","amsterdam","amsterdam", "utrecht", "den haag", "nijmegen", "den haag", "utrecht", "utrecht"])
+def pick_visitors(n):
+    with open("NL_demographics.csv", 'rt') as file:
+        rows = []
+        file = csv.reader(file, delimiter='\n', quotechar='|')
+        for i in file:
+            i = i[0].split(",")
+            i[1] = int(i[1])
+            rows.append(i)
+
+        inhabits = [float(row[1]) for row in rows]
+        total_inhabits = sum(inhabits)
+        probs = [x / float(total_inhabits) for x in inhabits]
+        cities = [city[0] for city in rows]
+        picked = np.random.choice(cities, n, p=probs)
+    return picked
+
+def count_occ(n):
+    city_count, counts = np.unique(pick_visitors(n), return_counts=True)
+    return zip(city_count, counts)
+
+# example call
+# print(count_occ(N_VISITORS))
