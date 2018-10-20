11fb7e4e4803ac0bb1320aa56747aac295a0de8a
Konstantin Todorov <kztodorov@outlook.com>
2018-10-20 02:07:23 +0200

    Added addressable-locations endpoint

---
+++ backend/google_api/google_api/gmaps_client.py
@@ -0,0 +1,89 @@
+import googlemaps
+from . import settings
+from .location import Location 
+from .responses.distance import Distance
+from .exceptions.invalid_use_error import InvalidUseError
+from .exceptions.server_error import ServerError
+
+class GMapsClient:
+    googleMapsClient = None
+
+    def __init__(self):
+        self.googleMapsClient = googlemaps.Client(key=settings.GOOGLE_MAPS_API_KEY)
+
+    def calculateDistance(self, location1, location2):
+        distanceMapMode = "driving"
+    
+        origin = (location1.latitude, location1.longitude)
+        destination = (location2.latitude, location2.longitude)
+        matrix = self.googleMapsClient.distance_matrix(origin, destination, mode=distanceMapMode)
+        
+        rowsKey = "rows"
+        elementsKey = "elements"
+        distanceKey = "distance"
+        durationKey = "duration"
+        valueKey = "value"
+
+        # make a check if the json returned from google api contains everything we need
+        if (matrix is None or rowsKey not in matrix or len(matrix[rowsKey]) <= 0 or 
+            elementsKey not in matrix[rowsKey][0] or len(matrix[rowsKey][0][elementsKey]) <= 0 or 
+            durationKey not in matrix[rowsKey][0][elementsKey][0] or 
+            distanceKey not in matrix[rowsKey][0][elementsKey][0] or 
+            valueKey not in matrix[rowsKey][0][elementsKey][0][durationKey] or 
+            valueKey not in matrix[rowsKey][0][elementsKey][0][distanceKey]):
+            return None
+        
+        meters = matrix[rowsKey][0][elementsKey][0][distanceKey][valueKey]
+        seconds = matrix[rowsKey][0][elementsKey][0][durationKey][valueKey]
+        if not meters or not seconds:
+            return None
+
+        kilometers = meters / 1000 # distance is in meters
+        minutes =  round(seconds / 60, 2) # duration is in seconds
+
+        return Distance(kilometers, minutes)
+
+    def getClosestAddressableLocations(self, latitude, longitude):
+        reverseGeocodeResults = []
+        try:
+            reverseGeocodeResults = self.googleMapsClient.reverse_geocode((latitude, longitude))
+        except googlemaps.exceptions.HTTPError as httpError:
+            if httpError.status_code == 400:
+                raise InvalidUseError("Invalid parameters ")
+            else:
+                raise ServerError("")
+
+        addresses = self.parseAddresses(reverseGeocodeResults)
+        return addresses
+
+    def parseAddresses(self, geocodeResults):
+        addressComponentsKey = "address_components"
+        cityLocationType = "locality"
+        secondLevelAdministrativeAreaType = "administrative_area_level_2"
+        firstLevelAdministrativeAreaType = "administrative_area_level_1"
+        # flatten the list of objects, each with list of address_components to one big list of all address_components
+        addressComponents = [item for sublist in [address[addressComponentsKey] for address in geocodeResults] for item in sublist]
+        cityAddresses = self.getAddressComponentsByType(addressComponents, cityLocationType)
+        if len(cityAddresses) > 0:
+            return cityAddresses
+
+        # if no cities are found, fall back to administrative areas of level 2
+        regionLevel2Addresses = self.getAddressComponentsByType(addressComponents, secondLevelAdministrativeAreaType)
+        if len(regionLevel2Addresses) > 0:
+            return regionLevel2Addresses
+            
+        # if no administrative areas of level 2 are found, fall back to administrative areas of level 1
+        regionLevel1Addresses = self.getAddressComponentsByType(addressComponents, firstLevelAdministrativeAreaType)
+        if len(regionLevel1Addresses) > 0:
+            return regionLevel1Addresses
+        
+    def getAddressComponentsByType(self, addressComponents, addressType):
+        typesKey = "types"
+        longNameKey = "long_name"
+
+        addresses = []
+        for addressComponent in addressComponents:
+            if addressType in addressComponent[typesKey]:
+                addresses.append(addressComponent[longNameKey])
+        
+        return list(set(addresses)) # this is done in order to distinct the values
--- backend/google_api/google_api/views.py
+++ backend/google_api/google_api/views.py
@@ -3,81 +3,48 @@ from django.shortcuts import render
 from django.http import HttpResponseNotAllowed, HttpResponseBadRequest, HttpResponseServerError, JsonResponse
 from .location import Location 
 from .responses.distance import Distance
-import googlemaps
+from .gmaps_client import GMapsClient
+from .exceptions.invalid_use_error import InvalidUseError
+from .exceptions.server_error import ServerError
 import json
-from . import settings
-
-# Create your views here.
-
-# def index(request):
-#     gmaps = googlemaps.Client(key='AIzaSyDXgDfHfSIf7pmZI7_MiANSJ9L2iD4lOE8')
-
-#     # Geocoding an address
-#     # geocode_result = gmaps.geocode('Variant Loodsenbouw')
-#     # print (geocode_result)
-
-#     # Look up an address with reverse geocoding
-#     reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
-#     return JsonResponse(reverse_geocode_result, safe=False)
 
 @csrf_exempt
 def getDistanceBetweenTwoPoints(request):
-    print ("received request")
-    if request.method == 'POST':
-        if not request.body:
-            return HttpResponseBadRequest("No body provided")
-
-        jsonData=json.loads(request.body)
-
-        location1Key = "location1"
-        location2Key = "location2"
-        if not jsonData or location1Key not in jsonData or location2Key not in jsonData:
-            return HttpResponseBadRequest("Invalid body provided")
-
-        location1 = Location(jsonData["location1"])
-        location2 = Location(jsonData["location2"])
-
-        distance = calculateDistanceBetweenTwoPoints(location1, location2)
-        if distance is None:
-            return HttpResponseServerError("")
-
-        return JsonResponse(distance.toJson(), safe=False)
-    else: #GET
+    if request.method != 'POST':
         return HttpResponseNotAllowed("")
-    # return JsonResponse("hello, world", safe=False)
 
-def getCitiesByCoordinates(request):
-    return JsonResponse("hello, world", safe=False)
+    if not request.body:
+        return HttpResponseBadRequest("No body provided")
 
-def calculateDistanceBetweenTwoPoints(location1, location2):
-    distanceMapMode = "driving"
-    
-    googleMapsClient = googlemaps.Client(key=settings.GOOGLE_MAPS_API_KEY)
-    origin = (location1.latitude, location1.longitude)
-    destination = (location2.latitude, location2.longitude)
-    matrix = googleMapsClient.distance_matrix(origin, destination, mode=distanceMapMode)
-    
-    rowsKey = "rows"
-    elementsKey = "elements"
-    distanceKey = "distance"
-    durationKey = "duration"
-    valueKey = "value"
+    jsonData=json.loads(request.body)
 
-    # make a check if the json returned from google api contains everything we need
-    if (matrix is None or rowsKey not in matrix or len(matrix[rowsKey]) <= 0 or 
-        elementsKey not in matrix[rowsKey][0] or len(matrix[rowsKey][0][elementsKey]) <= 0 or 
-        durationKey not in matrix[rowsKey][0][elementsKey][0] or 
-        distanceKey not in matrix[rowsKey][0][elementsKey][0] or 
-        valueKey not in matrix[rowsKey][0][elementsKey][0][durationKey] or 
-        valueKey not in matrix[rowsKey][0][elementsKey][0][distanceKey]):
-        return None
-    
-    meters = matrix[rowsKey][0][elementsKey][0][distanceKey][valueKey]
-    seconds = matrix[rowsKey][0][elementsKey][0][durationKey][valueKey]
-    if not meters or not seconds:
-        return None
+    location1Key = "location1"
+    location2Key = "location2"
+    if not jsonData or location1Key not in jsonData or location2Key not in jsonData:
+        return HttpResponseBadRequest("Invalid body provided")
 
-    kilometers = meters / 1000 # distance is in meters
-    minutes =  round(seconds / 60, 2) # duration is in seconds
+    location1 = Location(jsonData[location1Key])
+    location2 = Location(jsonData[location2Key])
 
-    return Distance(kilometers, minutes)
+    gmapsClient = GMapsClient()
+    distance = gmapsClient.calculateDistance(location1, location2)
+    if distance is None:
+        return HttpResponseServerError("")
+
+    return JsonResponse(distance.toJson(), safe=False)
+
+def getClosestAddressableLocationsByCoordinates(request):
+    gmapsClient = GMapsClient()
+
+    latitudeKey = "latitude"
+    longitudeKey = "longitude"
+    latitude = float(request.GET[latitudeKey])
+    longitude = float(request.GET[longitudeKey])
+
+    try:
+        closestAddressableLocations = gmapsClient.getClosestAddressableLocations(latitude, longitude)
+        return JsonResponse(closestAddressableLocations, safe=False)
+    except ServerError as serverError:
+        return HttpResponseServerError(str(serverError))
+    except InvalidUseError as invalidUseError:
+        return HttpResponseBadRequest(str(invalidUseError))
